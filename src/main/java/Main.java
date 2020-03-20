76e4c988faa0e4c107e17f7fa3825f933e708831
Ngu Nguyen <nguhnguyen@gmail.com>
2020-03-20 12:40:53 -0400

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,29 @@ public class Main {
     }
 
     private static void checkFrequency(int[] arr) {
+        int count = 0;
         int check = 4;
         for(int i = 0; i < arr.length; i++){
             boolean numberTheSame = checkTheNumber(check, arr[i]);
             if(numberTheSame){
-                System.out.print(arr[i] + " ");
+                String theSame = String.valueOf(arr[i]);
+                String theCheck = String.valueOf(check);
+                count = checkToSeeIfItIsDoubleNumber(count, theCheck, theSame);
             }
         }
+        System.out.println("\nThis is count: " + count);
     }
+
+    private static int checkToSeeIfItIsDoubleNumber(int count, String check, String theSame) {
+        String[] str = theSame.split("");
+        for(int i = 0; i < str.length; i++){
+            if(check.equals(str[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+
     private static boolean checkTheNumber(int check, int i) {
         while(i > 0){
             if(i % 10 == check){
